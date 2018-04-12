1b5a6c640e7e1bf8fc9f7db15a45e64caa4f81fd
Spencer Shaw <w0413731@selu.edu>
2018-04-12 14:45:51 -0500

    Left more notes. We are really close. Just need to figure out if we are creating the object correctly

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.listen(1069, function(err) {
 });
 
 // emr is a hard coded electronic medical record object. Buris still didn't email me about what the objects should contain 
-var emr = [{"id":0, "name":"John", "age":30, "health":"Good" },{"id":1, "name":"Jim", "age":25, "health":"Okay" }]
+//  var emr = new Array(1000) Do we need to init the array
+ emr = [{"id":0, "name":"John", "age":30, "health":"Good" },{"id":1, "name":"Jim", "age":25, "health":"Okay" }]
 
 //go to postman and type http://localhost:1069/api/emr and up will appear
 //So now we just have to have it grab some json obects and it will spit them out
@@ -38,15 +39,25 @@ app.get('/api/emr/:id', (req, res)=> {                  //with doing parameters 
     res.status(500).send("Not found")
 });
 
-app.post('/api/emr/create/:id/:age/:name/:health', (req, res)=> {
+app.post('/api/emr/create/:id/', (req, res)=> {
     var id = req.param('id');
-    var age = req.param('age');
-    var name = req.param('name');
-    var health = req.param('health');
+    // var age = req.param('age');
+    // var name = req.param('name');
+    // var health = req.param('health');
 
+
+
+    emr[2].id = id;     ////////////IMPORTANT It's passing in an ID. I know that much. However I cant create a new object to save my life. I can insert
+                        // The ID to an object that is already created no problem 
+    
+ 
+    // emr[2].name = name;
+    // emr[2].age = age;
+    // emr[2].health = health;
+    // so
 ///Just need to create an object here
 
-    res.status(200).send(id,age);
+    res.status(200).send(emr[2].id);
 });
 
 //Now we ju/api/emr/st have to store the objects in memory and make a POST request :)
@@ -54,6 +65,8 @@ app.post('/api/emr/create/:id/:age/:name/:health', (req, res)=> {
 
 
 //used as a POST reference
+
+
 // app.post('/login',function(req,res){
 //     var user_name=req.body.user;
 //     var password=req.body.password;
