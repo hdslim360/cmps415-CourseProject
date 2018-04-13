e39a1f6a1db586a806a8b1f4536f2fd32b0657db
Spencer Shaw <w0413731@selu.edu>
2018-04-13 16:47:34 -0500

    Changed port # to 5000

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ router.get('/test', function(req, res) {
 
 app.use('/api', router);
 
-app.listen(2000, function(err) {
+app.listen(5000, function(err) {
     if (err) {
         console.log(chalk.red(err));
     } else {
