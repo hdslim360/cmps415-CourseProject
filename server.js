d9d4d3e319a00eaa3e5b8000bca09029137c1806
Spencer Shaw <w0413731@selu.edu>
2018-04-13 17:57:16 -0500

    Latest

--- server.js
+++ server.js
@@ -13,62 +13,37 @@ app.listen(process.env.PORT ||5000, function(err) {
     if (err) {
         console.log(chalk.red(err));
     } else {
-        console.log(chalk.blue('Magic Happens on Port 1069'));
+        console.log(chalk.blue('Are you ready to rock!?!'));
     }
 });
 
-// emr is a hard coded electronic medical record object. Buris still didn't email me about what the objects should contain 
-//  var emr = new Array(1000) Do we need to init the array
  var emr = {"EMR":[{"id":0, "name":"John", "age":30, "health":"Good" },{"id":1, "name":"Jim", "age":25, "health":"Okay" }]};
 
-//go to postman and type http://localhost:1069/api/emr and up will appear
-//So now we just have to have it grab some json obects and it will spit them out
+//go to postman and type https://murmuring-reaches-97788.herokuapp.com/api/emr up will appear.  
+
 app.get('/api/emr', (req, res)=> {
     res.status(200).send(emr);
 });
 
 
-//in postman, type http://localhost:1069/api/emr/:Id/  IMPORTANT: this post on stack overflow really helped. The accepted answer really helped me 
-app.get('/api/emr/:id', (req, res)=> {                  //with doing parameters with postman https://stackoverflow.com/questions/37189780/how-to-send-http-url-parameters-in-postman
+//in postman, type https://murmuring-reaches-97788.herokuapp.com/api/emr/:id and add an int Id for the param
+app.get('/api/emr/:id', (req, res)=> {                  
     var userId = req.param('id')
      emr.EMR.find(item => item.id === userId);
     res.send(emr.EMR[userId]);
     res.status(500).send("Not found");
 });
 
+//in postman, type https://murmuring-reaches-97788.herokuapp.com/api/emr/create/:id/:age/:name/:health add params of course
+
 app.post('/api/emr/create/:id/:age/:name/:health', (req, res)=> {
-    //var str = JSON.parse(emr);
+
     var id = req.param('id');
     var age = req.param('age');
     var name = req.param('name');
     var health = req.param('health');
     emr.EMR.push({"id":id, "name":name, "age":age, "health":health });
     
-    //emr = JSON.stringify(str);
-
-    //emr[2].id = id;     ////////////IMPORTANT It's passing in an ID. I know that much. However I cant create a new object to save my life. I can insert
-                        // The ID to an object that is already created no problem 
-    
- 
-    // emr[2].name = name;
-    // emr[2].age = age;
-    // emr[2].health = health;
-    // so
-///Just need to create an object here
-
     res.status(200).send(emr);
 });
 
-//Now we ju/api/emr/st have to store the objects in memory and make a POST request :)
-
-
-
-//used as a POST reference
-
-
-// app.post('/login',function(req,res){
-//     var user_name=req.body.user;
-//     var password=req.body.password;
-//     console.log("User name = "+user_name+", password is "+password);
-//     res.end("yes");
-//   });
