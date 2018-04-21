f0218d60faca001ef0535179d2b05e881845e7f1
Spencer Shaw <w0413731@selu.edu>
2018-04-21 10:45:45 -0500

    last commit before merge

--- server.js
+++ server.js
@@ -17,36 +17,103 @@ app.listen(process.env.PORT ||5000, function(err) {
     } else {
         console.log(chalk.blue('Are you ready to rock!?!'));
     }
+    const mongodb = require('mongodb');
 
+    // Create seed data
     
-    const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-let seedData = {"EMR":[{"id":0, "name":"John", "age":30, "health":"Good","doctor":"Feel Good" },{"id":1, "name":"Jim", "age":25, "health":"Okay","doctor":"Evil" }]};
-
-// Connection URL
-const url = 'mongodb://heroku_llk6v74p:bd13o2m17duot0tjfpd2ad4r13@ds111565.mlab.com:11565/heroku_llk6v74p';
-
-// Database Name
-const dbName = 'MONGODB_URI';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  let emrData = db.collection('emrData')
-
-  emrData.insert(seedData, function(err, result){
+    let seedData = [
+      {   
+        decade: '1970s',   
+        artist: 'Debby Boone',   
+        song: 'You Light Up My Life',   
+        weeksAtOne: 10    
+      },   
+      {
+        decade: '1980s',
+        artist: 'Olivia Newton-John',
+        song: 'Physical',
+        weeksAtOne: 10
+      },
+      {
+        decade: '1990s',
+        artist: 'Mariah Carey',
+        song: 'One Sweet Day',    
+        weeksAtOne: 16
+      }    
+    ];
+    
+    // Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
+    
+    let uri = 'mongodb://heroku_llk6v74p:bd13o2m17duot0tjfpd2ad4r13@ds111565.mlab.com:11565/heroku_llk6v74p';
+    console.log('good1')
+    mongodb.MongoClient.connect(uri, function(err, client) {
+    
       if(err) throw err;
-  })
-
-  
-
-  client.close();
+      console.log('good2')
+      /*
+       * Get the database from the client. Nothing is required to create a
+       * new database, it is created automatically when we insert.
+       */
+    
+      let db = client.db('heroku_llk6v74p')
+    
+      /*
+       * First we'll add a few songs. Nothing is required to create the
+       * songs collection; it is created automatically when we insert.
+       */
+    
+      let songs = db.collection('songs');
+    
+       // Note that the insert method can take either an array or a dict.
+    
+      songs.insert(seedData, function(err, result) {
+    
+        if(err) throw err;
+    
+        /*
+         * Then we need to give Boyz II Men credit for their contribution
+         * to the hit "One Sweet Day".
+         */
+    
+        songs.update(
+          { song: 'One Sweet Day' },
+          { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
+          function (err, result) {
+    
+            if(err) throw err;
+    
+            /*
+             * Finally we run a query which returns all the hits that spend 10 or
+             * more weeks at number 1.
+             */
+    
+            songs.find({ weeksAtOne : { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
+    
+              if(err) throw err;
+    
+              docs.forEach(function (doc) {
+                console.log(
+                  'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] +
+                  ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
+                );
+              });
+    
+              // Since this is an example, we'll clean up after ourselves.
+              songs.drop(function (err) {
+                if(err) throw err;
+    
+                // Only close the connection when your app is terminating.
+                client.close(function (err) {
+                  if(err) throw err;
+                });
+              });
+            });
+          }
+        );
+      });
     });
+    
+    
 });
 
  var emr = {"EMR":[{"id":0, "name":"John", "age":30, "health":"Good","doctor":"Feel Good" },{"id":1, "name":"Jim", "age":25, "health":"Okay","doctor":"Evil" }]};
